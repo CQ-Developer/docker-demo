56ce5d936f186b8e5fb91fca6d7fc4c39e87ca96
chen <18551681083@163.com>
2023-06-29 18:06:55 +0800

    第三章部分脚本

---
+++ ch3.sh
@@ -0,0 +1,26 @@
+# 从备用注册表拉取镜像
+docker pull quay.io/dockerinaction/ch3_hello_registry:latest
+docker images -a
+
+# 删除镜像
+docker rmi quay.io/dockerinaction/ch3_hello_registry:latest
+
+# 将镜像导出未文件
+docker pull busybox:latest
+docker save -o myfile.tar busybox:latest
+
+# 从文件中加载镜像
+docker rmi busybox:latest
+docker load -i myfile.tar
+
+# 清理
+docker rmi busybox:latest
+rm myfile.tar
+
+# 从Dockerfile构建镜像
+git clone https://github.com/dockerinaction/ch3_dockerfile.git
+docker build -t dia_ch3/dockerfile:lastest ch3_dockerfile
+
+# 清理
+docker rmi dia_ch3/dockerfile:lastest
+rm -rf ch3_dockerfile
