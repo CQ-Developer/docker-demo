d2437a3fc185eb7fe76190aa9e38bb3c37746b24
chen <18551681083@163.com>
2023-07-06 17:21:06 +0800

    通过 docker run 命令的 -c,--cpu-shares 选项设置各容器间的CPU权重

--- ch6.sh
+++ ch6.sh
@@ -1,2 +1,5 @@
-# 启动数据库并通过-m或--memory选项限制容器的内存为256m
+# 启动 mariadb 并通过 -m,--memory 选项限制容器的最大可用内存为 256m
 docker run -d --name ch6_mariadb --memory 256m --cpu-shares 1024 --cap-drop net_raw -e MYSQL_ROOT_PASSWORD=test mariadb:5.5
+
+# 启动 wordpress 并限制进程的相对权重为 512
+docker run -d -P --name ch6_wordpress -m 512m --cpu-shares 512 --cap-drop net_raw --link ch6_mariadb:mysql -e WORDPRESS_DB_PASSWORD=test wordpress:5.0.0-php7.2-apache
