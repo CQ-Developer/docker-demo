f8c99979286a94559c6c54d31c909e61982c1055
chen <18551681083@163.com>
2023-07-12 11:07:49 +0800

    完成邮件发送客户端 详解 Dockerfile 的 ADD 指令

--- Dockerfile/mailer/ch8_2.sh
+++ Dockerfile/mailer/ch8_2.sh
@@ -7,3 +7,8 @@ docker inspect dockerinaction/mailer-base:0.6
 docker build -t dockerinaction/mailer-logging -f mailer-logging.df .
 # 运行容器
 docker run -d --name logging-mailer dockerinaction/mailer-logging
+
+# 构建镜像
+docker build -t dockerinaction/mailer-live -f mailer-live.df .
+# 启动容器
+docker run -d --name live-mailer dockerinaction/mailer-live
