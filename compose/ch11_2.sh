eb80aaf9a07b0992c29d58c7123d01ad80db53ff
chen <18551681083@163.com>
2023-07-17 23:09:51 +0800

    合并多余的描述

--- compose/ch11_2.sh
+++ compose/ch11_2.sh
@@ -1,9 +1,6 @@
 # 使用命令创建新栈并部署舒服
 # Docker栈是服务、卷、网络、密钥、配置的命令集合
-docker stack deploy -c databases.yml my-databases
-
-# 在更改YAML文件后
-# 再次执行该命令会更新服务
+# 更改YAML文件后，再次执行该命令会更新服务
 docker stack deploy -c databases.yml my-databases
 
 # 列出栈中所有的任务及其存在时间
