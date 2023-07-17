2e345afcf4c045bf49f31ebbbc6a0e77af280d93
chen <18551681083@163.com>
2023-07-17 23:01:37 +0800

    通过 docker stack ps 命令列出栈中任务

--- compose/ch11_2.sh
+++ compose/ch11_2.sh
@@ -1,6 +1,10 @@
 # 使用命令创建新栈并部署舒服
+# Docker栈是服务、卷、网络、密钥、配置的命令集合
 docker stack deploy -c databases.yml my-databases
 
 # 在更改YAML文件后
 # 再次执行该命令会更新服务
 docker stack deploy -c databases.yml my-databases
+
+# 列出栈中所有的任务及其存在时间
+docker stack ps --format '{{.Name}}\t{{.CurrentState}}' my-databases
